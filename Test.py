36d0f9f68907c6cd5eabcd447b55d5b15895ded1
gmozg <glebssa88@gmail.com>
2022-07-20 14:32:59 +0300

    Test

---
+++ Test.py
@@ -0,0 +1,28 @@
+x = 5
+
+if x == 0:
+    x += 1
+
+print(5/x)
+
+
+x = 'a'
+
+if x == 0:
+    x = 1
+    print('x был равен нулю')
+
+elif type(x) == type(5) or type(x) == type(5.5):
+    print('x допустимое значение')
+
+else:
+    print('В x не допустимый тип данных')
+    x = 1 
+
+
+print(100/x)
+
+
+
+
+     
